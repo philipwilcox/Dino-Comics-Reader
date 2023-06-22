37be1584bdb768249174189f3104e3c3b0f707c9
Phil <philip@philipwilcox.com>
2023-06-22 15:54:05 -0700

    extract remaining alt texts

--- Dino Comics Reader/WebView.swift
+++ Dino Comics Reader/WebView.swift
@@ -43,10 +43,16 @@ struct WebView: UIViewRepresentable {
 
                     let comic = try! doc.select("img.comic").first()!
                     let alt1 = try! comic.attr("title")
-
-                    self.secretTextFetcher(alt1, "THIS", "COOL")
+                    let contactQueryString = try! doc.select("a:contains(contact)").first()!.attr("href").split(separator: "?", maxSplits: 1, omittingEmptySubsequences: true)[1]
+                    let contactString = try! doc.select("a:contains(contact)").first()!.attr("href")
+                    let alt2 = URLComponents(string: contactString)!.queryItems!.first(where: { $0.name == "subject" })!.value!
+                    print(alt2)
+                    let commentElement = try! doc.select("body").first()?.getChildNodes().first(where: { $0.nodeName() == "#comment" })! as! Comment
+                    // if we ever had to find the right one from multiple comments instead of just the first, we could match on "<span class="rss-title">" in the comment
+                    let alt3 = try! SwiftSoup.parse(commentElement.getData()).select("span").first()!.text()
+                    
+                    self.secretTextFetcher(alt1, alt2, alt3)
                     }
-//                    print("LOADED")
             }
         }
     }
